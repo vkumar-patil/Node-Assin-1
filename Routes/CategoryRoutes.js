ae9d6cc4c8a4c181521d1a7a51e21487ea2379cc
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2024-10-01 14:53:07 +0530

    update category part

--- Routes/CategoryRoutes.js
+++ Routes/CategoryRoutes.js
@@ -4,4 +4,5 @@ const CategorisController = require("../controllers/CategorisController");
 router.post("/", CategorisController.CreateCategori);
 router.get("/", CategorisController.getallCategori);
 router.get("/:id", CategorisController.getcategoryId);
+router.put("/", CategorisController.updatecategory);
 module.exports = router;
--- controllers/CategorisController.js
+++ controllers/CategorisController.js
@@ -20,7 +20,28 @@ exports.getallCategori = (req, res) => {
 //single category
 
 exports.getcategoryId = (req, res) => {
-  const result = Categoris.find((e) => e.id === parent(req.params.id));
+  const result = Categoris.find((e) => e.id === parseInt(req.params.id));
   if (!result) return res.status(400).json({ massege: "result not found" });
   res.json(result);
+};
+
+//update category
+
+exports.updatecategory = (req, res) => {
+  const result = Categoris.find((cat) => cat.id === parseInt(req.params.id));
+  if (!result) res.status(404).json({ massege: "result not found" });
+  const { name } = req.body;
+  if (name) result.name = name;
+  res.json(result);
+};
+
+//delete category
+exports.deletecategory = (req, res) => {
+  const categoryIndex = Categoris.findIndex(
+    (cat) => cat.id !== parseInt(req.params.id)
+  );
+  if (categoryIndex == -1)
+    return res.status(404).json({ massege: "categoryIndex not found" });
+  Categoris.splice((categoryIndex, 1));
+  res.status(200).json({ massege: "success" });
 };
